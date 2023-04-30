24f04638ce8b595c59a1fb982c75f219b3cf9835
u7540836 <u7540836@anu.edu.au>
2023-04-30 12:50:44 +1000

    Update 01_regression_model.py

--- Python/01_regression_model.py
+++ Python/01_regression_model.py
@@ -9,5 +9,36 @@ Created on Sun Apr 30 10:29:39 2023
 """
 
 
+''' 1. IMPORT DATA '''
 
+'''
+Input  
+    1. start_date  
+    2. end_date 3. symbol 
+Output  
+    1. stock_data 
+
+Function
+    1. yf.download
+'''
+
+import yfinance as yf
+import matplotlib.pyplot as plt
+stock_data = yf.download("SPY", start = "2022-01-01", end = "2022-01-07")
+print(stock_data.head())
+
+''' 2. CREATE A ROLLING REGRESSION MODEL ''' 
+
+'''
+Input  
+    1. start_date  
+    2. end_date 3. symbol 
+Output  
+    1. stock_data 
+
+Function
+    1. yf.download
+'''
+
+''' 3. FIND THE PARAMETER FOR WINDOW_SIZE '''
 
