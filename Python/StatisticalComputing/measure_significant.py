8e3b0c7f2e8f8d110bc3d29a8a104c32806da9bf
u7540836 <u7540836@anu.edu.au>
2023-06-12 13:21:09 +1000

    Add Folder statistical computing

--- Python/StatisticalComputing/measure_significant.py
+++ Python/StatisticalComputing/measure_significant.py
@@ -10,10 +10,23 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import pandas as pd 
 np.random.seed(42)
-
+import sys
+from sklearn.utils import resample
 
 # 1. Permutation test
 def permutation_test(samples1, samples2, n_permutations):
+    """
+    Perform a permutation test to compare the means of two samples.
+
+    Parameters:
+        samples1 (array-like): The first sample.
+        samples2 (array-like): The second sample.
+        n_permutations (int): The number of permutations to perform.
+
+    Returns:
+        tuple: A tuple containing the observed difference of means and the p-value.
+
+    """
     # Compute the observed difference of means
     observed_diff = np.mean(samples2) - np.mean(samples1)
 
@@ -32,18 +45,18 @@ def permutation_test(samples1, samples2, n_permutations):
 
 '''
 # Example usage
-
 np.random.seed(42)
+
 n_samples = 50
 mean1, mean2 = 115.0, 120.0
 std_dev = 10.0
 n_permutations = 10000
+
 samples1 = np.random.normal(mean1, std_dev, n_samples)
 samples2 = np.random.normal(mean2, std_dev, n_samples)
 
- ----------------------------------------
- 
 observed_diff, p_value = permutation_test(samples1, samples2, n_permutations)
+
 print(f"Observed difference of means: {observed_diff:.4f}")
 print(f"P-value (permutation test): {p_value:.4f}")
 
@@ -62,24 +75,43 @@ plt.title('Normal Distributions')
 plt.show()
 '''
 
+# 2. Bootstrap: estimate uncertainty of mean or sd 
+def bootstrap_standard_error(data, n_iterations=1000):
+    means = []
+    for _ in range(n_iterations):
+        sample = resample(data)
+        means.append(np.mean(sample))
+
+    se = np.std(means, ddof=1)
+    return se
 
 
 
-# 2. Permutation test
+# Example
 
+'''
+# Read the CSV file and extract the vector of data
+csv_file = "/Users/nanthawat/Desktop/PythonExamPrep/Lecture/bootstrap_data.csv"
+data_column = 'V1'
+df = pd.read_csv(csv_file)
+data = df[data_column].values
 
+# Compute the bootstrap estimate of the standard error
+se_estimate = bootstrap_standard_error(data)
 
+print(f"Bootstrap estimate of the standard error: {se_estimate:.4f}")
 
+# Calculate the mean value
+mean_value = np.mean(data)
 
+# Create a bar plot with the mean value and standard error
+fig, ax = plt.subplots()
+ax.bar(0, mean_value, yerr=se_estimate, capsize=10)
+ax.set_xticks([0])
+ax.set_xticklabels(['Mean Value'])
+ax.set_title('Mean Value with Standard Error')
 
-
-
-
-
-
-
-
-
-
-
+# Show the plot
+plt.show()
+'''
 
